19021d4c2a9f8e8f3750a106c245f816a23b663d
Kamikazexxxl <kamikazexxxl@gmail.com>
2022-05-07 13:51:30 +0300

    Done

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,17 @@
 package app.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
-    private Long nanoTime = System.nanoTime();
+    private final Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
+    }
+
+    public Long getTimer() {
+        return getTime();
     }
 }
